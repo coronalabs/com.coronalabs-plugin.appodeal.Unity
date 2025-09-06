d1dd31f21d50816a85234f79419ea2a6d68ae671
Scott Harrison <scottrules44@gmail.com>
2025-09-06 17:16:02 -0400

    3.9.0.0 Update

--- plugins/2018.3326/iphone/UnityAds.framework/Headers/UnityAds.h
+++ plugins/2018.3326/iphone/UnityAds.framework/Headers/UnityAds.h
@@ -33,5 +33,14 @@ FOUNDATION_EXPORT const unsigned char UnityAdsModuleVersionString[];
 #import <UnityAds/USRVInitializeStateFactory.h>
 #import <UnityAds/USRVInitializeStateType.h>
 #import <UnityAds/UADSDeviceInfoProvider.h>
-
+#import <UnityAds/USRVStorageManager.h>
+#import <UnityAds/USRVStorage.h>
+#import <UnityAds/UADSGenericError.h>
+#import <UnityAds/UADSGenericCompletion.h>
+#import <UnityAds/UADSScarSignalParameters.h>
+#import <UnityAds/UADSGMAScarVersionBridge.h>
+#import <UnityAds/UADSGMAScarSignalsBridge.h>
+#import <UnityAds/UADSGMAScarAdsBridge.h>
+#import <UnityAds/GMAAdMetaData.h>
+#import <UnityAds/UADSWebViewEventSender.h>
 #endif /* UnityAds_h */
